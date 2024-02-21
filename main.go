558fa5e893d26e9c0d9ba335a80e6085df7fe530
Christian Gabriel D. Ramoso <123578310+ChristianGabrielDRamoso@users.noreply.github.com>
2024-02-21 15:32:31 +0800

    Removed Text  from struct

--- main.go
+++ main.go
@@ -13,35 +13,28 @@ import (
 
 type OvalDefinitions struct {
 	XMLName        xml.Name `xml:"oval_definitions" json:"oval_definitions,omitempty"`
-	Text           string   `xml:",chardata" json:"text,omitempty"`
 	SchemaLocation string   `xml:"schemaLocation,attr" json:"schemalocation,omitempty"`
 	Xmlns          string   `xml:"xmlns,attr" json:"xmlns,omitempty"`
 	Xsi            string   `xml:"xsi,attr" json:"xsi,omitempty"`
 	Oval           string   `xml:"oval,attr" json:"oval,omitempty"`
 	OvalDef        string   `xml:"oval-def,attr" json:"oval-def,omitempty"`
 	Generator      struct {
-		Text          string `xml:",chardata" json:"text,omitempty"`
 		ProductName   string `xml:"product_name"`
 		SchemaVersion string `xml:"schema_version"`
 		Timestamp     string `xml:"timestamp"`
 	} `xml:"generator" json:"generator,omitempty"`
 	Definitions struct {
-		Text       string `xml:",chardata" json:"text,omitempty"`
 		Definition []struct {
-			Text     string `xml:",chardata" json:"text,omitempty"`
 			ID       string `xml:"id,attr" json:"id,omitempty"`
 			Version  string `xml:"version,attr" json:"version,omitempty"`
 			Class    string `xml:"class,attr" json:"class,omitempty"`
 			Metadata struct {
-				Text     string `xml:",chardata" json:"text,omitempty"`
 				Title    string `xml:"title"`
 				Affected struct {
-					Text     string `xml:",chardata" json:"text,omitempty"`
 					Family   string `xml:"family,attr" json:"family,omitempty"`
 					Platform string `xml:"platform"`
 				} `xml:"affected" json:"affected,omitempty"`
 				Reference struct {
-					Text   string `xml:",chardata" json:"text,omitempty"`
 					RefID  string `xml:"ref_id,attr" json:"ref_id,omitempty"`
 					RefURL string `xml:"ref_url,attr" json:"ref_url,omitempty"`
 					Source string `xml:"source,attr" json:"source,omitempty"`
@@ -49,26 +42,20 @@ type OvalDefinitions struct {
 				Description string `xml:"description"`
 			} `xml:"metadata" json:"metadata,omitempty"`
 			Criteria struct {
-				Text      string `xml:",chardata" json:"text,omitempty"`
 				Operator  string `xml:"operator,attr" json:"operator,omitempty"`
 				Criterion []struct {
-					Text    string `xml:",chardata" json:"text,omitempty"`
 					TestRef string `xml:"test_ref,attr" json:"test_ref,omitempty"`
 					Comment string `xml:"comment,attr" json:"comment,omitempty"`
 				} `xml:"criterion" json:"criterion,omitempty"`
 				Criteria []struct {
-					Text      string `xml:",chardata" json:"text,omitempty"`
 					Operator  string `xml:"operator,attr" json:"operator,omitempty"`
 					Criterion []struct {
-						Text    string `xml:",chardata" json:"text,omitempty"`
 						TestRef string `xml:"test_ref,attr" json:"test_ref,omitempty"`
 						Comment string `xml:"comment,attr" json:"comment,omitempty"`
 					} `xml:"criterion" json:"criterion,omitempty"`
 					Criteria struct {
-						Text      string `xml:",chardata" json:"text,omitempty"`
 						Operator  string `xml:"operator,attr" json:"operator,omitempty"`
 						Criterion []struct {
-							Text    string `xml:",chardata" json:"text,omitempty"`
 							TestRef string `xml:"test_ref,attr" json:"test_ref,omitempty"`
 							Comment string `xml:"comment,attr" json:"comment,omitempty"`
 						} `xml:"criterion" json:"criterion,omitempty"`
@@ -78,28 +65,22 @@ type OvalDefinitions struct {
 		} `xml:"definition" json:"definition,omitempty"`
 	} `xml:"definitions" json:"definitions,omitempty"`
 	Tests struct {
-		Text        string `xml:",chardata" json:"text,omitempty"`
 		RpminfoTest []struct {
-			Text    string `xml:",chardata" json:"text,omitempty"`
 			ID      string `xml:"id,attr" json:"id,omitempty"`
 			Version string `xml:"version,attr" json:"version,omitempty"`
 			Comment string `xml:"comment,attr" json:"comment,omitempty"`
 			Check   string `xml:"check,attr" json:"check,omitempty"`
 			Xmlns   string `xml:"xmlns,attr" json:"xmlns,omitempty"`
 			Object  struct {
-				Text      string `xml:",chardata" json:"text,omitempty"`
 				ObjectRef string `xml:"object_ref,attr" json:"object_ref,omitempty"`
 			} `xml:"object" json:"object,omitempty"`
 			State struct {
-				Text     string `xml:",chardata" json:"text,omitempty"`
 				StateRef string `xml:"state_ref,attr" json:"state_ref,omitempty"`
 			} `xml:"state" json:"state,omitempty"`
 		} `xml:"rpminfo_test" json:"rpminfo_test,omitempty"`
 	} `xml:"tests" json:"tests,omitempty"`
 	Objects struct {
-		Text          string `xml:",chardata" json:"text,omitempty"`
 		RpminfoObject []struct {
-			Text    string `xml:",chardata" json:"text,omitempty"`
 			ID      string `xml:"id,attr" json:"id,omitempty"`
 			Version string `xml:"version,attr" json:"version,omitempty"`
 			Xmlns   string `xml:"xmlns,attr" json:"xmlns,omitempty"`
@@ -107,18 +88,14 @@ type OvalDefinitions struct {
 		} `xml:"rpminfo_object" json:"rpminfo_object,omitempty"`
 	} `xml:"objects" json:"objects,omitempty"`
 	States struct {
-		Text         string `xml:",chardata" json:"text,omitempty"`
 		RpminfoState []struct {
-			Text        string `xml:",chardata" json:"text,omitempty"`
 			ID          string `xml:"id,attr" json:"id,omitempty"`
 			AttrVersion string `xml:"version,attr" json:"version,omitempty"`
 			Xmlns       string `xml:"xmlns,attr" json:"xmlns,omitempty"`
 			Version     struct {
-				Text      string `xml:",chardata" json:"text,omitempty"`
 				Operation string `xml:"operation,attr" json:"operation,omitempty"`
 			} `xml:"version" json:"version,omitempty"`
 			Evr struct {
-				Text      string `xml:",chardata" json:"text,omitempty"`
 				Datatype  string `xml:"datatype,attr" json:"datatype,omitempty"`
 				Operation string `xml:"operation,attr" json:"operation,omitempty"`
 			} `xml:"evr" json:"evr,omitempty"`
