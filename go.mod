3605824885d1772b56eacfc26a74f9c0f52b3603
Christian Gabriel D. Ramoso <123578310+ChristianGabrielDRamoso@users.noreply.github.com>
2024-02-21 14:36:36 +0800

    Added Files from working prototype build

---
+++ go.mod
@@ -0,0 +1,5 @@
+module obiwan
+
+go 1.22.0
+
+require golang.org/x/net v0.21.0
---
+++ main.go
@@ -0,0 +1,247 @@
+package main
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"path/filepath"
+	"strings"
+	"sync"
+)
+
+type OvalDefinitions struct {
+	XMLName        xml.Name `xml:"oval_definitions" json:"oval_definitions,omitempty"`
+	Text           string   `xml:",chardata" json:"text,omitempty"`
+	SchemaLocation string   `xml:"schemaLocation,attr" json:"schemalocation,omitempty"`
+	Xmlns          string   `xml:"xmlns,attr" json:"xmlns,omitempty"`
+	Xsi            string   `xml:"xsi,attr" json:"xsi,omitempty"`
+	Oval           string   `xml:"oval,attr" json:"oval,omitempty"`
+	OvalDef        string   `xml:"oval-def,attr" json:"oval-def,omitempty"`
+	Generator      struct {
+		Text          string `xml:",chardata" json:"text,omitempty"`
+		ProductName   string `xml:"product_name"`
+		SchemaVersion string `xml:"schema_version"`
+		Timestamp     string `xml:"timestamp"`
+	} `xml:"generator" json:"generator,omitempty"`
+	Definitions struct {
+		Text       string `xml:",chardata" json:"text,omitempty"`
+		Definition []struct {
+			Text     string `xml:",chardata" json:"text,omitempty"`
+			ID       string `xml:"id,attr" json:"id,omitempty"`
+			Version  string `xml:"version,attr" json:"version,omitempty"`
+			Class    string `xml:"class,attr" json:"class,omitempty"`
+			Metadata struct {
+				Text     string `xml:",chardata" json:"text,omitempty"`
+				Title    string `xml:"title"`
+				Affected struct {
+					Text     string `xml:",chardata" json:"text,omitempty"`
+					Family   string `xml:"family,attr" json:"family,omitempty"`
+					Platform string `xml:"platform"`
+				} `xml:"affected" json:"affected,omitempty"`
+				Reference struct {
+					Text   string `xml:",chardata" json:"text,omitempty"`
+					RefID  string `xml:"ref_id,attr" json:"ref_id,omitempty"`
+					RefURL string `xml:"ref_url,attr" json:"ref_url,omitempty"`
+					Source string `xml:"source,attr" json:"source,omitempty"`
+				} `xml:"reference" json:"reference,omitempty"`
+				Description string `xml:"description"`
+			} `xml:"metadata" json:"metadata,omitempty"`
+			Criteria struct {
+				Text      string `xml:",chardata" json:"text,omitempty"`
+				Operator  string `xml:"operator,attr" json:"operator,omitempty"`
+				Criterion []struct {
+					Text    string `xml:",chardata" json:"text,omitempty"`
+					TestRef string `xml:"test_ref,attr" json:"test_ref,omitempty"`
+					Comment string `xml:"comment,attr" json:"comment,omitempty"`
+				} `xml:"criterion" json:"criterion,omitempty"`
+				Criteria []struct {
+					Text      string `xml:",chardata" json:"text,omitempty"`
+					Operator  string `xml:"operator,attr" json:"operator,omitempty"`
+					Criterion []struct {
+						Text    string `xml:",chardata" json:"text,omitempty"`
+						TestRef string `xml:"test_ref,attr" json:"test_ref,omitempty"`
+						Comment string `xml:"comment,attr" json:"comment,omitempty"`
+					} `xml:"criterion" json:"criterion,omitempty"`
+					Criteria struct {
+						Text      string `xml:",chardata" json:"text,omitempty"`
+						Operator  string `xml:"operator,attr" json:"operator,omitempty"`
+						Criterion []struct {
+							Text    string `xml:",chardata" json:"text,omitempty"`
+							TestRef string `xml:"test_ref,attr" json:"test_ref,omitempty"`
+							Comment string `xml:"comment,attr" json:"comment,omitempty"`
+						} `xml:"criterion" json:"criterion,omitempty"`
+					} `xml:"criteria" json:"criteria,omitempty"`
+				} `xml:"criteria" json:"criteria,omitempty"`
+			} `xml:"criteria" json:"criteria,omitempty"`
+		} `xml:"definition" json:"definition,omitempty"`
+	} `xml:"definitions" json:"definitions,omitempty"`
+	Tests struct {
+		Text        string `xml:",chardata" json:"text,omitempty"`
+		RpminfoTest []struct {
+			Text    string `xml:",chardata" json:"text,omitempty"`
+			ID      string `xml:"id,attr" json:"id,omitempty"`
+			Version string `xml:"version,attr" json:"version,omitempty"`
+			Comment string `xml:"comment,attr" json:"comment,omitempty"`
+			Check   string `xml:"check,attr" json:"check,omitempty"`
+			Xmlns   string `xml:"xmlns,attr" json:"xmlns,omitempty"`
+			Object  struct {
+				Text      string `xml:",chardata" json:"text,omitempty"`
+				ObjectRef string `xml:"object_ref,attr" json:"object_ref,omitempty"`
+			} `xml:"object" json:"object,omitempty"`
+			State struct {
+				Text     string `xml:",chardata" json:"text,omitempty"`
+				StateRef string `xml:"state_ref,attr" json:"state_ref,omitempty"`
+			} `xml:"state" json:"state,omitempty"`
+		} `xml:"rpminfo_test" json:"rpminfo_test,omitempty"`
+	} `xml:"tests" json:"tests,omitempty"`
+	Objects struct {
+		Text          string `xml:",chardata" json:"text,omitempty"`
+		RpminfoObject []struct {
+			Text    string `xml:",chardata" json:"text,omitempty"`
+			ID      string `xml:"id,attr" json:"id,omitempty"`
+			Version string `xml:"version,attr" json:"version,omitempty"`
+			Xmlns   string `xml:"xmlns,attr" json:"xmlns,omitempty"`
+			Name    string `xml:"name"`
+		} `xml:"rpminfo_object" json:"rpminfo_object,omitempty"`
+	} `xml:"objects" json:"objects,omitempty"`
+	States struct {
+		Text         string `xml:",chardata" json:"text,omitempty"`
+		RpminfoState []struct {
+			Text        string `xml:",chardata" json:"text,omitempty"`
+			ID          string `xml:"id,attr" json:"id,omitempty"`
+			AttrVersion string `xml:"version,attr" json:"version,omitempty"`
+			Xmlns       string `xml:"xmlns,attr" json:"xmlns,omitempty"`
+			Version     struct {
+				Text      string `xml:",chardata" json:"text,omitempty"`
+				Operation string `xml:"operation,attr" json:"operation,omitempty"`
+			} `xml:"version" json:"version,omitempty"`
+			Evr struct {
+				Text      string `xml:",chardata" json:"text,omitempty"`
+				Datatype  string `xml:"datatype,attr" json:"datatype,omitempty"`
+				Operation string `xml:"operation,attr" json:"operation,omitempty"`
+			} `xml:"evr" json:"evr,omitempty"`
+		} `xml:"rpminfo_state" json:"rpminfo_state,omitempty"`
+	} `xml:"states" json:"states,omitempty"`
+}
+
+func main() {
+	url := "https://ftp.suse.com/pub/projects/security/oval/"
+	files, err := fetchFiles(url)
+	if err != nil {
+		panic(err)
+	}
+
+	var wg sync.WaitGroup
+	for _, file := range files {
+		wg.Add(1)
+		go func(file string) {
+			defer wg.Done()
+			err := processFile(file)
+			if err != nil {
+				fmt.Printf("Error processing %s: %s\n", file, err)
+			}
+		}(file)
+	}
+	wg.Wait()
+	fmt.Println("All files processed successfully.")
+}
+
+func fetchFiles(url string) ([]string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to fetch files: %s", resp.Status)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var files []string
+	links := extractLinks(string(body))
+	for _, link := range links {
+		if strings.HasSuffix(link, ".xml") {
+			files = append(files, url+link)
+		}
+	}
+
+	return files, nil
+}
+
+func extractLinks(html string) []string {
+	var links []string
+	startIndex := 0
+	for {
+		linkStart := strings.Index(html[startIndex:], "href=\"")
+		if linkStart == -1 {
+			break
+		}
+		linkStart += startIndex + len("href=\"")
+		linkEnd := strings.Index(html[linkStart:], "\"")
+		if linkEnd == -1 {
+			break
+		}
+		linkEnd += linkStart
+		links = append(links, html[linkStart:linkEnd])
+		startIndex = linkEnd
+	}
+	return links
+}
+
+func processFile(url string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("failed to download file: %s", resp.Status)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	// Extract filename from URL
+	filename := filepath.Base(url)
+
+	// Write the XML content to a file
+	err = ioutil.WriteFile(filename, body, 0644)
+	if err != nil {
+		return err
+	}
+
+	// Unmarshal XML content
+	var ovalDefinitions OvalDefinitions
+	err = xml.Unmarshal(body, &ovalDefinitions)
+	if err != nil {
+		return err
+	}
+
+	// Create JSON filename
+	jsonFilename := strings.TrimSuffix(filename, filepath.Ext(filename)) + ".json"
+
+	// Marshal OvalDefinitions to JSON
+	jsonData, err := json.MarshalIndent(ovalDefinitions, "", "    ")
+	if err != nil {
+		return err
+	}
+
+	// Write JSON content to a file
+	err = ioutil.WriteFile(jsonFilename, jsonData, 0644)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("File %s processed successfully.\n", filename)
+	return nil
+}
